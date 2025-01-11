18947123083dbbace3c2b09125d784a489fd3cf6
istiakAHMEDsaad <istik2020@gmail.com>
2025-01-11 11:24:58 +0600

    done footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -56,33 +56,33 @@ const Footer = () => {
     <footer className="section">
         <div className="container">
             
-            <div className="">
-                <div className="">
-                    <h1 className="headline-1">Let&apos;s codding together today!</h1>
+            <div className="lg:grid lg:grid-cols-2">
+                <div className="mb-10">
+                    <h1 className="headline-1 mb-8 lg:max-w-[90%]">Let&apos;s code together today!</h1>
 
                     <ButtonPrimary href="mailto:istiakahmedsaad.official@gmail.com" label="Get started" icon="chevron_right" />
                 </div>
 
-                <div className="">
+                <div className="grid grid-cols-2 gap-4 lg:pl-20">
                     <div>
-                        <p className="">Sitemap</p>
+                        <p className="mb-2">Sitemap</p>
 
                         <ul>
                             {sitemap.map(({label, href}, idx)=>(
                                 <li key={idx}>
-                                    <a href={href} className="">{label}</a>
+                                    <a href={href} className="block text-sm text-zinc-400 py-1 transition-colors hover:text-zinc-200">{label}</a>
                                 </li>
                             ))}
                         </ul>
                     </div>
 
                     <div>
-                        <p className="">Socials</p>
+                        <p className="mb-2">Socials</p>
 
                         <ul>
                             {socials.map(({label, href}, idx)=>(
                                 <li key={idx}>
-                                    <a href={href} target="_blank" className="">{label}</a>
+                                    <a href={href} target="_blank" className="block text-sm text-zinc-400 py-1 transition-colors hover:text-zinc-200">{label}</a>
                                 </li>
                             ))}
                         </ul>
@@ -90,13 +90,13 @@ const Footer = () => {
                 </div>
             </div>
 
-            <div className="">
+            <div className="flex items-center justify-between pt-10 mb-8">
                 <a href="" className="">
                     <img src="/white.png" height={40} width={40} alt="logo" />
                 </a>
 
-                <p className="">
-                    &copy; 2025 <span className="">istiakAHMEDsaad</span>
+                <p className="text-zinc-500 text-sm">
+                    &copy; 2025 <span className="text-zinc-200">istiakAHMEDsaad</span>
                 </p>
             </div>
 
